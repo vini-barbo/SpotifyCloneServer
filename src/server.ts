d7b25643e1967726aa895dad4592ac5aa0fca713
Vinicius Barbosa Dos Santos <62946143+vini-barbo@users.noreply.github.com>
2024-01-22 10:50:18 -0300

    connection with database has been established

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,31 @@
-import express , { Application , Request, Response } from "express";
-
+import express, { Application, Request, Response } from "express";
+import Database from "./config/database";
 class App {
     public app: Application;
 
-    constructor(){
-        this.app = express()
+    constructor() {
+        this.app = express();
+        this.routes()
+        this.databaseSync()
     }
 
-    protected routes():void{
-        this.app.route('/').get((req:Request, res:Response) => {
+    protected databaseSync(): void {
+        const db = new Database();
+        db.sequelize?.sync()
+    }
+
+    protected routes(): void {
+        this.app.route('/').get((req: Request, res: Response) => {
             res.send("welcome home")
         })
     }
 }
 
-const port: number = Number(process.env.PORT)
+const port: number = Number(process.env.SERVER_PORT)
 
 const app = new App().app
 
-console.log('running?')
 
 app.listen(port, () => {
-    console.log('server running')
+    console.log('✅ Server Connection has been create successfully at port', port)
 })
