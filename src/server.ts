b6dbc943e1ae1a0795d172c5b9d70e03e1cda5ef
Vinicius Barbosa Dos Santos <62946143+vini-barbo@users.noreply.github.com>
2024-01-24 12:25:51 -0300

    Refactor

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,7 @@ class Server {
     private SERVER_PORT: number = Number(process.env.SERVER_PORT)
     private app = new App().app
 
-    constructor() {
-        this.start()
-    }
-
-    private async start() {
+    public async start() {
         this.app.listen(this.SERVER_PORT, () => {
             console.log('✅  Server Connection has been create successfully at port', this.SERVER_PORT)
         })
@@ -16,3 +12,4 @@ class Server {
 }
 
 const server = new Server()
+server.start()
