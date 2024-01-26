057cb378fd80d91195b862c22255e172a63cdb6b
Vinicius Barbosa Dos Santos <62946143+vini-barbo@users.noreply.github.com>
2024-01-26 02:35:37 -0300

    Refactor

--- src/app.ts
+++ src/app.ts
@@ -9,26 +9,8 @@ class App {
     constructor() {
         this.app = express();
         this.routes()
-        this.databaseSync()
     }
 
-    protected async databaseSync() {
-        console.log('Criando corno')
-
-        // const pessoa1 = await
-        //     prisma.user.create({
-        //         data: {
-        //             birthdate: new Date,
-        //             email: 'teste@hotmail.com',
-        //             first_name: 'jose',
-        //             last_name: 'teste',
-        //             password: 'Teste123213',
-        //             user_name: 'testeDoTeste'
-        //         }
-        //     })
-
-        // console.log(await prisma.user.findFirst())
-    }
 
     protected routes(): void {
         this.app.route('/').get((req: Request, res: Response) => {
